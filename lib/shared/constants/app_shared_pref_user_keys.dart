ba6c33db7e2e6fdb6ff027d0c0f0cc171ab28e86
madz <markdiesta@gmail.com>
2020-07-05 04:12:02 +0800

    Create app_shared_pref_user_keys.dart

---
+++ lib/shared/constants/app_shared_pref_user_keys.dart
@@ -0,0 +1,8 @@
+class AppSharedPrefUserKeys {
+  static const String USER_ID_KEY = "TOUTLY_USER_ID_KEY";
+  static const String USER_EMAIL_KEY = "TOUTLY_USER_EMAIL_KEY";
+  static const String USER_GEOLOCATION_LATITUDE_KEY =
+      "TOUTLY_USER_GEOLOCATION_LATITUDE_KEY";
+  static const String USER_GEOLOCATION_LONGITUDE_KEY =
+      "TOUTLY_USER_GEOLOCATION_LONGITUDE_KEY";
+}
