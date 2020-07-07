4b6b7dbeffe9c72f92e24d87cbbf72ce15328f82
madz <markdiesta@gmail.com>
2020-07-08 03:14:27 +0800

    Update authentication_bloc.dart

--- lib/features/authentication/bloc/authentication_bloc.dart
+++ lib/features/authentication/bloc/authentication_bloc.dart
@@ -1,12 +1,8 @@
 import 'dart:async';
 
-import 'package:Toutly/core/usecases/auth/firebase_get_user_usecase.dart';
 import 'package:Toutly/core/usecases/auth/firebase_is_signedin_usecase.dart';
 import 'package:Toutly/core/usecases/auth/firebase_signout_use_case.dart';
-import 'package:Toutly/core/usecases/local_shared_pref/local_shared_pref_delete_all_save_data_usecase.dart';
-import 'package:Toutly/core/usecases/local_shared_pref/local_shared_pref_persist_user_geo_location_usecase.dart';
 import 'package:Toutly/core/usecases/param/use_case_no_param.dart';
-import 'package:Toutly/core/usecases/param/user/use_case_user_param.dart';
 import 'package:Toutly/core/usecases/user/firestore_get_user_usecase.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -16,28 +12,17 @@ part 'authentication_bloc.freezed.dart';
 part 'authentication_event.dart';
 part 'authentication_state.dart';
 
-/// bloc for Authentication
 @lazySingleton
 class AuthenticationBloc
     extends Bloc<AuthenticationEvent, AuthenticationState> {
   final FirebaseIsSignedInUserUseCase firebaseIsSignedInUserUseCase;
-  final FirebaseGetUserUseCase firebaseGetUserUseCase;
   final FirebaseSignOutUserUseCase firebaseSignOutUserUseCase;
-
   final FirestoreGetUserUseCase firestoreGetUserUseCase;
 
-  final LocalSharedPrefPersistUserGeoLocationUseCase
-      localSharedPrefPersistUserGeoLocation;
-  final LocalSharedDeleteAllSaveDataUseCase localSharedDeleteAllSaveData;
-
-  /// create Authentication bloc
   AuthenticationBloc({
     @required this.firebaseIsSignedInUserUseCase,
-    @required this.firebaseGetUserUseCase,
     @required this.firebaseSignOutUserUseCase,
     @required this.firestoreGetUserUseCase,
-    @required this.localSharedPrefPersistUserGeoLocation,
-    @required this.localSharedDeleteAllSaveData,
   }) : super(AuthenticationState.initial());
 
   @override
@@ -55,25 +40,9 @@ class AuthenticationBloc
         }
       },
       signedIn: (e) async* {
-        final firebaseUser =
-            await firebaseGetUserUseCase.call(UseCaseNoParam.init());
-
-        final userModel =
-            await firestoreGetUserUseCase.call(UseCaseUserParamUserId.init(
-          firebaseUser.uid,
-        ));
-
-        /// Saved geolocation data on the local.
-        localSharedPrefPersistUserGeoLocation
-            .call(UseCaseUserParamGeoLocation.init(
-          userModel.geoLocation?.latitude ?? 0,
-          userModel.geoLocation?.longitude ?? 0,
-        ));
-
         yield AuthenticationState.authenticated();
       },
       signedOut: (e) async* {
-        localSharedDeleteAllSaveData.call(UseCaseNoParam.init());
         await firebaseSignOutUserUseCase.call(UseCaseNoParam.init());
         yield AuthenticationState.unauthenticated();
       },
