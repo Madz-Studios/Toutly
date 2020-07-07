c5cc4f018a043ffd73bce6d5ded7e445e66bc37e
madz <markdiesta@gmail.com>
2020-07-08 03:26:11 +0800

    Remove open location and address.

--- lib/features/inbox/screen/inbox_screen.dart
+++ lib/features/inbox/screen/inbox_screen.dart
@@ -1,11 +1,5 @@
-import 'package:Toutly/shared/bloc/remote_config_data/remote_config_data_bloc.dart';
-import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:google_map_location_picker/google_map_location_picker.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class InboxScreen extends StatefulWidget {
   @override
@@ -14,8 +8,6 @@ class InboxScreen extends StatefulWidget {
 
 class _InboxScreenState extends State<InboxScreen> {
   final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
-
-  String _currentAddress = 'Unknown';
 
   @override
   void initState() {
@@ -31,55 +23,12 @@ class _InboxScreenState extends State<InboxScreen> {
             child: Column(
               children: <Widget>[
                 Text('Inbox Screen'),
-                Text(_currentAddress != null ? _currentAddress : 'Unknown'),
                 Placeholder(),
-                BlocBuilder<RemoteConfigDataBloc, RemoteConfigDataState>(
-                  builder: (context, state) {
-                    return RaisedButton(
-                      child: Text('Open Location Map'),
-                      onPressed: () async {
-                        print('Open Location Map');
-                        _getCurrentLocation(context, state.firebaseApiKey);
-                      },
-                    );
-                  },
-                ),
               ],
             ),
           ),
         ),
       ),
     );
-  }
-
-  _getCurrentLocation(BuildContext context, String firebaseApiKey) async {
-    try {
-      Position position = await geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low);
-
-      print('latitude ${position.latitude}');
-      print('longitude ${position.longitude}');
-
-      LocationResult result = await showLocationPicker(
-        context,
-        firebaseApiKey,
-        initialCenter: LatLng(position.latitude, position.longitude),
-        myLocationButtonEnabled: true,
-        hintText: 'Location',
-      );
-
-      print('result $result');
-
-      setState(() {
-        _currentAddress = "${result?.address ?? ''}";
-      });
-    } on PlatformException catch (platFormException) {
-      if (platFormException.code == 'PERMISSION_DENIED') {
-        AppSettings.openLocationSettings();
-      } else {
-        print('platFormException code ${platFormException.code}');
-        print('platFormException message ${platFormException.message}');
-      }
-    }
   }
 }
