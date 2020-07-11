f2706b4f52fae3e25089a7f0a224e1c347c41786
madz <markdiesta@gmail.com>
2020-07-11 16:23:51 +0800

    Update photos_carousel.dart

--- lib/features/home/widgets/photos_carousel.dart
+++ lib/features/home/widgets/photos_carousel.dart
@@ -1,6 +1,6 @@
 import 'package:Toutly/core/models/algolia/algolia_barter_model.dart';
+import 'package:Toutly/shared/util/cached_images.dart';
 import 'package:Toutly/shared/widgets/carousel/indicator_carousel.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
@@ -21,10 +21,10 @@ class _PhotosCarouselState extends State<PhotosCarousel> {
   int _current = 0;
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
       children: [
         CarouselSlider(
-          items: _getCachedImages(widget.algoliaBarter.photosUrl),
+          items: getCachedImages(widget.algoliaBarter.photosUrl),
           options: CarouselOptions(
             viewportFraction: 1.0,
             onPageChanged: (index, reason) {
@@ -34,39 +34,11 @@ class _PhotosCarouselState extends State<PhotosCarousel> {
             },
           ),
         ),
-        Align(
-          alignment: Alignment.topLeft,
-          child: IndicatorCarousel(
-            items: _getCachedImages(widget.algoliaBarter.photosUrl),
-            current: _current,
-          ),
+        IndicatorCarousel(
+          items: getCachedImages(widget.algoliaBarter.photosUrl),
+          current: _current,
         ),
       ],
     );
-  }
-
-  List<Widget> _getCachedImages(List<String> photosUrl) {
-    List<Widget> listPhoto = [];
-    for (final image in photosUrl) {
-      listPhoto.add(
-        CachedNetworkImage(
-          fit: BoxFit.cover,
-          imageUrl: image == null ? '' : image,
-          imageBuilder: (context, imageProvider) => Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              image: DecorationImage(
-                image: imageProvider,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          placeholder: (context, url) => Container(),
-          errorWidget: (context, url, error) => Icon(Icons.error),
-        ),
-      );
-    }
-
-    return listPhoto;
   }
 }
