497b7e2a99ee819a78ccff57bda84a2b630e36a7
madz <markdiesta@gmail.com>
2020-07-16 18:28:48 +0800

    Trade screen

--- lib/features/trade_offer/screen/trade_offer_screen.dart
+++ lib/features/trade_offer/screen/trade_offer_screen.dart
@@ -1,13 +1,23 @@
 import 'package:Toutly/core/models/barter/barter_model.dart';
+import 'package:Toutly/core/models/user/user_model.dart';
+import 'package:Toutly/features/trade_offer/widgets/barter_item_card.dart';
+import 'package:Toutly/shared/util/app_size_config.dart';
 import 'package:Toutly/shared/widgets/buttons/back_or_close_button.dart';
+import 'package:Toutly/shared/widgets/profile_with_rating.dart';
 import 'package:flutter/material.dart';
 
 class TradeOfferScreen extends StatelessWidget {
   final BarterModel barterModel;
+  final UserModel barterUser;
 
-  TradeOfferScreen(this.barterModel);
+  TradeOfferScreen({
+    @required this.barterModel,
+    @required this.barterUser,
+  });
   @override
   Widget build(BuildContext context) {
+    final appSizeConfig = AppSizeConfig(context);
+
     return Scaffold(
       appBar: AppBar(
         leading: BackOrCloseButton(
@@ -21,52 +31,210 @@ class TradeOfferScreen extends StatelessWidget {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            BarterItemCard(barterModel),
-          ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: appSizeConfig.blockSizeHorizontal * 2,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: appSizeConfig.blockSizeVertical * 2,
+              ),
+              BarterItemCard(barterModel),
+              SizedBox(
+                height: appSizeConfig.blockSizeVertical * 4,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: appSizeConfig.blockSizeHorizontal * 4,
+                ),
+                child: Text(
+                  'Make your offer!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              SizedBox(
+                height: appSizeConfig.blockSizeVertical * 2,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: appSizeConfig.blockSizeHorizontal * 4,
+                ),
+                child: Text(
+                  'More chane of winning this item when you write a personal message. '
+                  'You may also ask questions to the person.',
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 12.0,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              SizedBox(
+                height: appSizeConfig.blockSizeVertical * 2,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: appSizeConfig.blockSizeHorizontal * 4,
+                ),
+                child: ProfileWithRating(
+                  user: barterUser,
+                ),
+              ),
+              SizedBox(
+                height: appSizeConfig.blockSizeVertical * 2,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: appSizeConfig.blockSizeHorizontal * 2,
+                ),
+                child: _TextArea(),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: appSizeConfig.blockSizeHorizontal * 4,
+                ),
+                child: Text(
+                  'Note: Cash offer is not allowed!',
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 12.0,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              SizedBox(
+                height: appSizeConfig.blockSizeVertical * 4,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: appSizeConfig.blockSizeHorizontal * 4,
+                ),
+                child: Text(
+                  'Select your item',
+                  style: TextStyle(
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              SizedBox(
+                height: appSizeConfig.blockSizeVertical * 1,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: appSizeConfig.blockSizeHorizontal * 4,
+                ),
+                child: Text(
+                  'Select the item you want to trade for from our item list',
+                  style: TextStyle(
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 12.0,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              SizedBox(
+                height: appSizeConfig.blockSizeVertical * 2,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: appSizeConfig.blockSizeHorizontal * 4,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: appSizeConfig.blockSizeVertical * 7.5,
+                      width: appSizeConfig.blockSizeHorizontal * 17.5,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Color(0XFFB4B4B4),
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            8.0,
+                          ),
+                        ),
+                      ),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.add,
+                          color: Color(0XFFB4B4B4),
+                          size: appSizeConfig.blockSizeVertical * 4,
+                        ),
+                        onPressed: () {
+//                        _showAddPhotoOptions(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class BarterItemCard extends StatelessWidget {
-  final BarterModel barterModel;
-  BarterItemCard(this.barterModel);
+class _TextArea extends StatefulWidget {
+  const _TextArea({
+    Key key,
+  }) : super(key: key);
 
   @override
+  __TextAreaState createState() => __TextAreaState();
+}
+
+class __TextAreaState extends State<_TextArea> {
+  final _controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return Card(
-        child: Row(
-      children: [
-        Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(
-              Radius.circular(
-                8.0,
-              ),
-            ),
-            image: DecorationImage(
-              image: NetworkImage(barterModel.photosUrl[0]),
-              fit: BoxFit.cover,
-            ),
+    return TextFormField(
+      controller: _controller,
+      maxLines: 6,
+      keyboardType: TextInputType.multiline,
+      maxLength: 500,
+      textAlign: TextAlign.left,
+      style: TextStyle(
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w300,
+        fontSize: 12.0,
+      ),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Color(0XFFF7F5F5),
+        hintText: 'Write a message..',
+        hintStyle: TextStyle(
+          fontStyle: FontStyle.normal,
+          fontWeight: FontWeight.normal,
+          fontSize: 12.0,
+          color: Colors.black,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.transparent),
+          borderRadius: BorderRadius.all(
+            Radius.circular(8.0),
           ),
         ),
-        Column(
-          children: [
-            Text(
-              '${barterModel.title}',
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 16.0,
-              ),
-            )
-          ],
-        )
-      ],
-    ));
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.transparent),
+          borderRadius: BorderRadius.all(
+            Radius.circular(8.0),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/trade_offer/widgets/barter_item_card.dart
@@ -0,0 +1,110 @@
+import 'package:Toutly/core/models/barter/barter_model.dart';
+import 'package:Toutly/shared/util/app_size_config.dart';
+import 'package:flutter/material.dart';
+
+class BarterItemCard extends StatelessWidget {
+  final BarterModel barterModel;
+  BarterItemCard(this.barterModel);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: CustomListItem(
+        thumbnail: barterModel.photosUrl[0],
+        title: barterModel.title,
+        description: barterModel.description,
+      ),
+    );
+  }
+}
+
+class CustomListItem extends StatelessWidget {
+  const CustomListItem({
+    this.thumbnail,
+    this.title,
+    this.description,
+  });
+
+  final String thumbnail;
+  final String title;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    final appSizeConfig = AppSizeConfig(context);
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.all(5.0),
+              height: appSizeConfig.blockSizeVertical * 10,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    8.0,
+                  ),
+                ),
+                image: DecorationImage(
+                  image: NetworkImage(thumbnail),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: _BarterItemDescription(
+              title: title,
+              description: description,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BarterItemDescription extends StatelessWidget {
+  const _BarterItemDescription({
+    Key key,
+    this.title,
+    this.description,
+  }) : super(key: key);
+
+  final String title;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    final appSizeConfig = AppSizeConfig(context);
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0,
+            ),
+          ),
+          SizedBox(
+            height: appSizeConfig.blockSizeVertical * 1,
+          ),
+          Text(
+            description,
+            style: TextStyle(fontSize: 12.0),
+          ),
+          SizedBox(
+            height: appSizeConfig.blockSizeVertical * 1,
+          )
+        ],
+      ),
+    );
+  }
+}
