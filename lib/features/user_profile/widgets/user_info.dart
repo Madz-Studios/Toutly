83bc0e187db7fb13e02bd18fb037d4538208ef8c
madz <markdiesta@gmail.com>
2020-08-14 11:03:52 +0800

    Remove user rating in user profile.
    Fixed https://github.com/Madz-Studios/Toutly/issues/24

--- lib/features/user_profile/widgets/user_info.dart
+++ lib/features/user_profile/widgets/user_info.dart
@@ -2,7 +2,6 @@ import 'package:Toutly/shared/bloc/user/user_bloc.dart';
 import 'package:Toutly/shared/util/app_size_config.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class UserInfo extends StatelessWidget {
   final UserState userState;
@@ -37,22 +36,22 @@ class UserInfo extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              RatingBar(
-                ignoreGestures: true,
-                initialRating: userState.userModel?.userRating == null
-                    ? 0
-                    : userState.userModel.userRating,
-                minRating: 0,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 5,
-                itemSize: appSizeConfig.blockSizeVertical * 2,
-                itemBuilder: (context, _) => Icon(
-                  Icons.star,
-                  color: Colors.amber,
-                ),
-                onRatingUpdate: (value) {},
-              ),
+//              RatingBar(
+//                ignoreGestures: true,
+//                initialRating: userState.userModel?.userRating == null
+//                    ? 0
+//                    : userState.userModel.userRating,
+//                minRating: 0,
+//                direction: Axis.horizontal,
+//                allowHalfRating: true,
+//                itemCount: 5,
+//                itemSize: appSizeConfig.blockSizeVertical * 2,
+//                itemBuilder: (context, _) => Icon(
+//                  Icons.star,
+//                  color: Colors.amber,
+//                ),
+//                onRatingUpdate: (value) {},
+//              ),
               Text(
                 userState.userModel?.address ?? '',
                 overflow: TextOverflow.fade,
